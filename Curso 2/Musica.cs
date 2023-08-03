1055bd0cc8e7f99099d47442f6d83a0c4fd01134
jvolive <joaooliveira1999@outlook.com>
2023-08-03 14:42:16 -0300

    Aula 1 - Curso 2

---
+++ Curso 2/Musica.cs
@@ -0,0 +1,24 @@
+class Musica //aula 1
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponivel no plano.");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano Plus+");
+        }
+
+
+    }
+}
---
+++ Curso 2/Program.cs
@@ -0,0 +1,14 @@
+﻿Musica musica1 = new Musica();//aula 1
+musica1.nome = "God`s plan";
+musica1.artista = "Drake";
+musica1.duracao = 237;
+musica1.disponivel = true;
+
+Musica musica2 = new Musica();
+musica2.nome = "Fear of the dark";
+musica2.artista = "Iron Maiden";
+musica2.duracao = 437;
+musica2.disponivel = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
