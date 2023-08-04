b8a0e67d578fa6c9ac2c777e38d08ab510a9b6a9
jvolive <joaooliveira1999@outlook.com>
2023-08-03 22:43:57 -0300

    Aula 2 - Curso 2

--- Curso 2/Program.cs
+++ Curso 2/Program.cs
@@ -1,14 +1,14 @@
 ﻿Musica musica1 = new Musica();//aula 1
-musica1.nome = "God`s plan";
-musica1.artista = "Drake";
-musica1.duracao = 237;
-musica1.disponivel = true;
+musica1.Nome = "God`s plan";
+musica1.Artista = "Drake";
+musica1.Duracao = 237;
+musica1.Disponivel = true;
 
 Musica musica2 = new Musica();
-musica2.nome = "Fear of the dark";
-musica2.artista = "Iron Maiden";
-musica2.duracao = 437;
-musica2.disponivel = false;
+musica2.Nome = "Fear of the dark";
+musica2.Artista = "Iron Maiden";
+musica2.Duracao = 437;
+musica2.Disponivel = false;
 
 musica1.ExibirFichaTecnica();
 musica2.ExibirFichaTecnica();
